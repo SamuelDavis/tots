26150723f2069e7fb53350d4e3c8f471cfdbd5c2
Samuel Davis <samueljakdavis@gmail.com>
2024-11-02 19:25:16 -0400

    collect 4 apples; styling; win state

--- src/App.tsx
+++ src/App.tsx
@@ -1,118 +1,212 @@
 import {
   createEffect,
-  createMemo,
   createSignal,
+  For,
+  JSX,
   onCleanup,
   onMount,
+  Show,
 } from "solid-js";
+import { createStore, produce } from "solid-js/store";
 
 type Point = { x: number; y: number };
+type Input = (typeof knownKeys)[number];
+type State = {
+  player: Point & { hasApple: boolean };
+  apple: Point;
+  box: Point & { apples: number };
+  done: boolean;
+};
 
+const knownKeys = ["w", "a", "s", "d"] as const;
 const inputLength = 3;
 const speed = 200;
-
-function getPositionStyles(position: Point) {
-  return {
-    "--x": position.x,
-    "--y": position.y,
-  };
-}
+const targetApples = 4;
+const width = 5;
+const height = 10;
 
 function isEqual(a: Point, b: Point): boolean {
   return a.x === b.x && a.y === b.y;
 }
 
-function App() {
-  const [getPlayerHasApple, setPlayerHasApple] = createSignal(false);
-  const [getAnimating, setAnimating] = createSignal(false);
-  const [getInput, setInput] = createSignal<Point[]>([]);
-  const [getPlayerPosition, setPlayerPosition] = createSignal<Point>({
-    x: 0,
-    y: 0,
-  });
-  const [getApplePosition, setApplePosition] = createSignal<Point>({
-    x: 5,
-    y: 2,
-  });
-  const [getBoxPosition] = createSignal<Point>({
-    x: 2,
-    y: 7,
-  });
-  const getPlayerStyles = createMemo(() => ({
-    ...getPositionStyles(getPlayerPosition()),
-    "--speed": speed,
-  }));
-  const getAppleStyles = createMemo(() =>
-    getPositionStyles(getApplePosition()),
-  );
-  const getBoxStyles = createMemo(() => getPositionStyles(getBoxPosition()));
-  const move = (delta: Point) =>
-    setInput((prev) => [...prev, delta].slice(-inputLength));
+function rand(max = Number.MAX_VALUE, min = 0): number {
+  return Math.floor(Math.random() * max + min) - min;
+}
 
-  createEffect(() => {
-    if (getInput().length === 0) return;
+function positionToCss(position: Point): JSX.CSSProperties {
+  return { "--x": position.x, "--y": position.y };
+}
+
+function norm(n: number) {
+  return Math.min(1, Math.max(-1, n));
+}
+
+function inputToDelta(input: Input): Point {
+  switch (input) {
+    case "w":
+      return { x: 0, y: -1 };
+    case "d":
+      return { x: +1, y: 0 };
+    case "s":
+      return { x: 0, y: +1 };
+    case "a":
+      return { x: -1, y: 0 };
+  }
+}
+
+function App() {
+  const [input, setInput] = createStore<Input[]>([]);
+  const [getAnimating, setAnimating] = createSignal(false);
+  const [state, setState] = createStore<State>({
+    player: { x: 0, y: 0, hasApple: false },
+    box: { x: rand(width), y: rand(height), apples: 0 },
+    apple: { x: rand(width), y: rand(height) },
+    done: false,
+  });
+
+  function GameLoop() {
+    if (state.done) return;
+    if (input.length === 0) return;
     if (getAnimating()) return;
     setAnimating(true);
-    const [delta, ...rest] = getInput();
-    setPlayerPosition((position) => ({
-      x: position.x + delta.x,
-      y: position.y + delta.y,
-    }));
+    const [next, ...rest] = input;
     setInput(rest);
+    setState(
+      produce((state) => {
+        const delta = inputToDelta(next);
+
+        if (state.player.x + delta.x < 0) return;
+        if (state.player.y + delta.y < 0) return;
+
+        state.player.x += delta.x;
+        state.player.y += delta.y;
+        state.player.hasApple =
+          state.player.hasApple || isEqual(state.player, state.apple);
+
+        if (state.player.hasApple) {
+          state.apple.x = state.player.x;
+          state.apple.y = state.player.y;
+        }
+
+        if (state.player.hasApple && isEqual(state.player, state.box)) {
+          state.player.hasApple = false;
+          state.box.apples += 1;
+          do {
+            state.apple.x = rand(width);
+            state.apple.y = rand(height);
+          } while (isEqual(state.apple, state.box));
+        }
+
+        if (state.box.apples >= targetApples) {
+          state.player.hasApple = false;
+          state.player.y += 1;
+          state.done = true;
+        }
+      }),
+    );
     setTimeout(() => setAnimating(false), speed);
-  });
-
-  createEffect(() => {
-    const player = getPlayerPosition();
-    const apple = getApplePosition();
-    setPlayerHasApple((hasApple) => hasApple || isEqual(player, apple));
-  });
-
-  createEffect(() => {
-    if (getPlayerHasApple()) setApplePosition(getPlayerPosition());
-  });
-
-  createEffect(() => {
-    if (getPlayerHasApple() && isEqual(getPlayerPosition(), getBoxPosition())) {
-      setPlayerHasApple(false);
-    }
-  });
-
-  function onKeyDown(e: KeyboardEvent) {
-    switch (e.key) {
-      case "w":
-        move({ x: 0, y: -1 });
-        break;
-      case "d":
-        move({ x: 1, y: 0 });
-        break;
-      case "s":
-        move({ x: 0, y: +1 });
-        break;
-      case "a":
-        move({ x: -1, y: 0 });
-        break;
-    }
   }
 
-  function onKeyUp() {
+  function addInput(key: string): void {
+    setInput((keys) =>
+      knownKeys.includes(key as Input)
+        ? ([...keys, key].slice(-inputLength) as Input[])
+        : keys,
+    );
+  }
+
+  function onKeyDown({ key }: KeyboardEvent): void {
+    addInput(key);
+  }
+
+  function onKeyUp(): void {
     setInput([]);
   }
 
+  const [, setPointer] = createSignal<undefined | Point>();
+
+  function onPointerDown(event: PointerEvent) {
+    setPointer({ x: event.clientX, y: event.clientY });
+  }
+
+  function onPointerUp(event: PointerEvent) {
+    setPointer((pointer) => {
+      if (pointer) {
+        const dx = event.clientX - pointer.x;
+        const dy = event.clientY - pointer.y;
+        if (Math.abs(dy) > Math.abs(dx))
+          switch (norm(dy)) {
+            case -1:
+              addInput("w");
+              break;
+            case +1:
+              addInput("s");
+              break;
+          }
+        if (Math.abs(dx) > Math.abs(dy))
+          switch (norm(dx)) {
+            case -1:
+              addInput("a");
+              break;
+            case +1:
+              addInput("d");
+              break;
+          }
+      }
+      return undefined;
+    });
+  }
+
+  createEffect(GameLoop);
+
   onMount(() => {
+    document.documentElement.style.setProperty("--speed", speed.toString());
     window.addEventListener("keydown", onKeyDown);
     window.addEventListener("keyup", onKeyUp);
+    window.addEventListener("pointerdown", onPointerDown);
+    window.addEventListener("pointerup", onPointerUp);
+    setState(
+      produce((state) => {
+        do {
+          state.apple.x = rand(width);
+          state.apple.y = rand(height);
+        } while (
+          isEqual(state.apple, state.box) ||
+          isEqual(state.apple, state.player)
+        );
+      }),
+    );
   });
   onCleanup(() => {
     window.removeEventListener("keydown", onKeyDown);
     window.removeEventListener("keyup", onKeyUp);
+    window.removeEventListener("pointerdown", onPointerDown);
+    window.removeEventListener("pointerup", onPointerUp);
   });
 
   return (
     <main>
-      <div class="box positioned" style={getBoxStyles()} />
-      <div class="player positioned" style={getPlayerStyles()} />
-      <div class="apple positioned" style={getAppleStyles()} />
+      <div class="box positioned" style={positionToCss(state.box)}>
+        <For each={Array(state.box.apples)}>{() => <div class="apple" />}</For>
+      </div>
+      <div class="player positioned" style={positionToCss(state.player)} />
+      <Show
+        when={!state.done}
+        fallback={
+          <dialog open>
+            <h1>You Win!</h1>
+          </dialog>
+        }
+      >
+        <Show
+          when={state.player.hasApple}
+          fallback={
+            <div class="apple positioned" style={positionToCss(state.apple)} />
+          }
+        >
+          <div class="apple positioned" style={positionToCss(state.apple)} />
+        </Show>
+      </Show>
     </main>
   );
 }
